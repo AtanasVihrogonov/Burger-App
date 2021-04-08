eb4ed093a35531f4375a04f582bbc51ee8d27ab3
AV <vihrogonov@protonmail.com>
2021-04-08 11:31:53 +0100

    Adding Reusable Navigation Item & Creating Responsive Sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Logo from '../../Logo/Logo';
 
+import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
+import NavigationItems from '../NovigationItems/NavigationItems'
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>...</nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
